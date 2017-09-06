3d7d0a8d6d358b41da7ff6b585fcf74d0ddbfd25
snakeful <fulblind@gmail.com>
2017-09-06 14:32:41 -0600

    Add info to projects component. SHow info in html of projects component.

--- src/app/project/projects/projects.component.ts
+++ src/app/project/projects/projects.component.ts
@@ -6,7 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
-
+  projects = [{
+    id: 1,
+    name: 'Project 1',
+    desc: 'This is a project template'
+  }, {
+    id: 2,
+    name: 'Project 2',
+    desc: 'This is a project template'
+  }, {
+    id: 3,
+    name: 'Project 3',
+    desc: 'This is a project template'
+  }];
   constructor() { }
 
   ngOnInit() {
