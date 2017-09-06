d2f68bb29f7af1ed283b910bb3dfe8498a59005b
snakeful <fulblind@gmail.com>
2017-09-06 14:27:44 -0600

    Add home, project and stories component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
+import { ProjectsComponent } from './project/projects/projects.component';
+import { StoriesComponent } from './project/stories/stories.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    HomeComponent,
+    FooterComponent,
+    ProjectsComponent,
+    StoriesComponent
   ],
   imports: [
     BrowserModule
