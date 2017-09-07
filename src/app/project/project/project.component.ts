507a99fcf16b2076bdc3a3e0742ac2535119f57e
snakeful <fulblind@gmail.com>
2017-09-06 23:25:35 -0600

    Add data to test project component.

--- src/app/project/project/project.component.ts
+++ src/app/project/project/project.component.ts
@@ -14,6 +14,80 @@ export class ProjectComponent implements OnInit {
 
   ngOnInit() {
     this.project = this.service.getProject(parseInt(this.route.snapshot.params.id || 0));
+    this.project.userStories = [{
+      id: 1,
+      name: 'User Story 1',
+      desc: 'This is a user story for testing purposes',
+      priorityId: 0,
+      statusId: 0,
+      finalTasks: []
+    }, {
+      id: 2,
+      name: 'User Story 2',
+      desc: 'This is a user story for testing purposes',
+      priorityId: 0,
+      statusId: 0,
+      finalTasks: []
+    }, {
+      id: 3,
+      name: 'User Story 3',
+      desc: 'This is a user story for testing purposes',
+      priorityId: 0,
+      statusId: 0,
+      finalTasks: []
+    }];
+    this.project.sprints = [{
+      id: 1,
+      name: 'Sprint #1',
+      start: new Date(),
+      end: new Date(),
+      userStories: this.project.userStories,
+      dailyScrum: [{
+        date: new Date(),
+        start: new Date(0),
+        end: new Date(0),
+        observations: ''
+      }, {
+        date: new Date(),
+        start: new Date(0),
+        end: new Date(0),
+        observations: ''
+      }]
+    }, {
+      id: 1,
+      name: 'Sprint #2',
+      start: new Date(),
+      end: new Date(),
+      userStories: this.project.userStories,
+      dailyScrum: [{
+        date: new Date(),
+        start: new Date(0),
+        end: new Date(0),
+        observations: ''
+      }, {
+        date: new Date(),
+        start: new Date(0),
+        end: new Date(0),
+        observations: ''
+      }]
+    }, {
+      id: 1,
+      name: 'Sprint #3',
+      start: new Date(),
+      end: new Date(),
+      userStories: this.project.userStories,
+      dailyScrum: [{
+        date: new Date(),
+        start: new Date(0),
+        end: new Date(0),
+        observations: ''
+      }, {
+        date: new Date(),
+        start: new Date(0),
+        end: new Date(0),
+        observations: ''
+      }]
+    }];
   }
 
 }
