e483760f2ac4a05288ad603c144106628932c12e
snakeful <fulblind@gmail.com>
2017-09-15 21:56:50 -0600

    Add users to projects.

--- src/app/project/projects/projects.component.ts
+++ src/app/project/projects/projects.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ProjectsService, Project } from '../../services/projects/projects.service';
+import { ProjectsService, User, Project } from '../../services/projects/projects.service';
 
 @Component({
   selector: 'app-projects',
@@ -8,6 +8,9 @@ import { ProjectsService, Project } from '../../services/projects/projects.servi
 })
 export class ProjectsComponent implements OnInit {
   private _projects: Project[];
+  private _productOwnerUsers: User[];
+  private _scrumMasterUsers: User[];
+  private _scrumTeamUsers: User[];
   constructor(private projectsService: ProjectsService) {
   }
 
@@ -16,6 +19,32 @@ export class ProjectsComponent implements OnInit {
       .then((projects) => {
         this._projects = projects;
       });
+
+    this.projectsService.getProductOwnerUsers()
+      .then(users => {
+        this._productOwnerUsers = users;
+      });
+
+    this.projectsService.getScrumMasterUsers()
+      .then(users => {
+        this._scrumMasterUsers = users;
+      });
+
+    this.projectsService.getScrumTeamUsers()
+      .then(users => {
+        this._scrumTeamUsers = users;
+      });
+  }
+
+  onChangeUser(project) {
+    let user = this._scrumTeamUsers[project.selectedUser];
+    if (project.scrumTeam.indexOf(user) === -1) {
+      project.scrumTeam.push(user);
+    }
+  }
+
+  doRemoveUser(project, user) {
+    project.scrumTeam.splice(project.scrumTeam.indexOf(user), 1);
   }
 
   get projects(): Project[] {
@@ -24,5 +53,17 @@ export class ProjectsComponent implements OnInit {
 
   set projects(value: Project[]) {
     this._projects = value;
+  }
+
+  get productOwnerUsers(): User[] {
+    return this._productOwnerUsers;
+  }
+
+  get scrumMasterUsers(): User[] {
+    return this._scrumMasterUsers;
+  }
+
+  get scrumTeamUsers(): User[] {
+    return this._scrumTeamUsers;
   }
 }
