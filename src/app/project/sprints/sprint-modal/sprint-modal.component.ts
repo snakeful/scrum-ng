57186206a34deaf864c57ac7102f9c30e751fb8a
snakeful <fulblind@gmail.com>
2017-10-25 00:01:11 -0600

    :bug: Fix dates on sprints.

--- src/app/project/sprints/sprint-modal/sprint-modal.component.ts
+++ src/app/project/sprints/sprint-modal/sprint-modal.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
+import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
+
 import { Sprint } from '../../../services/shared/projects/projects.service';
 
 @Component({
@@ -10,10 +12,12 @@ import { Sprint } from '../../../services/shared/projects/projects.service';
 export class SprintModalComponent implements OnInit, AfterViewInit {
   private _sprint: Sprint;
   private _saveSprint: EventEmitter<any>;
+  private _dates: Date[];
   @ViewChild('dataSprintModalClose') private btnClose: ElementRef;
   constructor() {
     this._sprint = new Sprint(undefined, 'Test');
     this._saveSprint = new EventEmitter<any>();
+    this._dates = [new Date(), new Date()];
   }
 
   ngOnInit() {
@@ -23,6 +27,8 @@ export class SprintModalComponent implements OnInit, AfterViewInit {
   }
 
   doSaveSprint(sprint: Sprint) {
+    sprint.start = this._dates[0];
+    sprint.end = this._dates[1];
     this._saveSprint.emit({
       sprint: sprint,
       btnClose: this.btnClose
@@ -41,9 +47,24 @@ export class SprintModalComponent implements OnInit, AfterViewInit {
       this._sprint.start = new Date();
       this._sprint.end = new Date();
     }
+    this.dates = [new Date(this._sprint.start.valueOf()), new Date(this._sprint.end.valueOf())];
   }
 
   @Output() get saveSprint(): EventEmitter<any> {
     return this._saveSprint;
+  }
+
+  get dateConfig(): Partial<BsDatepickerConfig> {
+    return {
+      containerClass: 'theme-dark-blue'
+    };
+  }
+ 
+  get dates(): Date[] {
+    return this._dates;
+  }
+ 
+  set dates(dates: Date[]) {
+    this._dates = dates;
   }
 }
